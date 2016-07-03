dca95763b2a0298c009457c532a31fbb09c39482
ColinHwang <chwang7158@gmail.com>
2016-07-03 20:26:38 +0800

    Initial Commit

---
+++ iOS-RoundNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-RoundNumber
+//
+//  Created by colin on 16/7/3.
+//  Copyright © 2016年 CHwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-RoundNumber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-RoundNumber
+//
+//  Created by colin on 16/7/3.
+//  Copyright © 2016年 CHwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
